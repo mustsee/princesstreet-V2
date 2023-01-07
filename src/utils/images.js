053dee8d5f5eb254628aadcb5c895b3b36fe8db0
Thomas Sypniewski <thomas@thomass-macbook-pro.home>
2023-01-07 21:36:26 +0100

    Cleanup SlideShow widget, importing images differently and making it work with GH (github pages)

--- src/utils/images.js
+++ src/utils/images.js
@@ -33,7 +33,6 @@ export const findImage = async (imagePath) => {
   // Start from public folder to go back to src/images...
   // Is needed to make it work with github pages
   if (process.env.NODE_ENV !== "development") {
-    console.log("d", imagePath.replace("~/", "/../src/"));
     return { src: imagePath.replace("~/", "/../src/") };
   } else {
     const images = await fetchLocalImages();
