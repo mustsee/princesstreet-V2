b48389b66f395a0504c6026b3a2b239efc68fe3d
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2023-02-11 15:34:15 +0100

    Analytics, cname, url for official website

--- src/config.mjs
+++ src/config.mjs
@@ -2,20 +2,18 @@ export const SITE = {
   name: "Princes Street Hostel",
 
   // For test
-  origin: "https://humournews.com",
+  origin: "https://princesstreethostel.com",
   basePathname: "/",
   // For github pages
   //origin: "https://mustsee.github.io",
   //basePathname: "/princesstreet-V2/",
-  //origin: "https://princesstreethostel.com",
-  //basePathname: "/",
   //trailingSlash: false, // false when it will be without basePathname (cf. sitemap)
 
   title: "Princes Street Hostel",
   description:
     "🏨 Princes Street is a clean, safe and well located Hostel at the heart of Edinburgh. We are next to the Waverley train station, in a breeze from the Old Town. Come experience Edinburgh with us!",
 
-  googleAnalyticsId: "G-5LSB28K2VF", // or "G-XXXXXXXXXX",
+  googleAnalyticsId: "G-FMEJ6MK8VR", // or "G-XXXXXXXXXX",
   googleSiteVerificationId: false, // or "orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M",
 };
 
